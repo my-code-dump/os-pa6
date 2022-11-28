f724009a0dd52aa914e0fdf4c5c17404b9b83c65
Hieu <hquang@unm.edu>
2022-11-28 01:16:12 -0700

    Updated main

--- main.c
+++ main.c
@@ -1,15 +1,48 @@
 #include <stdio.h> 
 #include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
 #include <pthread.h>
+#include "random437.h"
+#define MAXWAITPEOPLE 800
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int line = 0;
 
 void* ready_queue() {
+    int incoming = poissonRandom(25); 
+    pthread_mutex_lock(&mutex);
+    if (line <= MAXWAITPEOPLE) {
+        line += incoming;
+    }
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+
+void* car(int MAXPERCAR) {
+    pthread_mutex_lock(&mutex);
+    if ((line - MAXPERCAR) >= 0) {
+        line -= MAXPERCAR; 
+    }
+    pthread_mutex_unlock(&mutex);
 
     return NULL;
 }
 
 int main (int argc, char** argv) {
-    int n = 4;
-    pthread_t tid[n + 1];
+    int CARNUM = 4;
+    int MAXPERCAR = 7;
+    pthread_t tid[CARNUM + 1];
+    pthread_create(&tid[0], NULL, ready_queue, NULL);
+
+    for (int i = 1; i <= CARNUM; i++) {
+        pthread_create(&tid[i], NULL, car(MAXPERCAR), (void *)&i);
+    }
+
+    for (int i = 0; i <= CARNUM; i++) {
+        pthread_join(tid[i],NULL);
+    }
 
     return 0;
 }
