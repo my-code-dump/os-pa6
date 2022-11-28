889ef891e3b187ffdd14ba2c6f57bf2e37818d1b
Hieu Quang <hquang@unm.edu>
2022-11-27 20:22:59 -0700

    Update: Added a method

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h> 
+#include <stdlib.h>
+#include <pthread.h>
+
+void* ready_queue() {
+
+    return NULL;
+}
 
 int main (int argc, char** argv) {
+    int n = 4;
+    pthread_t tid[n + 1];
+
     return 0;
 }
